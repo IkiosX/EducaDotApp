3925b93eb19542167d83fc0d681d413fc0bec265
Mohabbat <mohabbat@Mohabbats-MacBook-Air.local>
2024-04-16 18:38:52 +0100

    updated svg colour

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './nav.css';
-import logo from '../../assets/convertedPng.svg'
+import logo from '../../assets/convertedpng1 (1).svg'
 const Navbar = () => {
   return (
     <nav className="navbar">
