306998bf99e5715c493dc0e3b05552657d08343a
Mohabbat Hossain Khan <115047607+IkiosX@users.noreply.github.com>
2024-04-28 11:12:10 +0100

    Update setUpTests.js

--- src/setUpTests.js
+++ src/setUpTests.js
@@ -1,7 +1,7 @@
 // src/setUpTests.js
 import FirebaseMock from 'firebase-mock';
 
-// Mock the Firebase services you use
+
 const mockAuth = new FirebaseMock.MockAuthentication();
 const mockFirestore = new FirebaseMock.MockFirestore();
 
@@ -10,12 +10,8 @@ jest.mock('firebase/app', () => {
   return {
     auth: () => mockAuth,
     firestore: () => mockFirestore,
-    // Include other Firebase services as needed
+    
   };
 });
 
-// Reset the mocks before each test
-beforeEach(() => {
-  mockAuth.autoFlush();
-  mockFirestore.autoFlush();
-});
+
