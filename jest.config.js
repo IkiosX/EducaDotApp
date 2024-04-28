2490040c47ac152ce8b96ce55bfadd2d972a305a
Mohabbat Hossain Khan <115047607+IkiosX@users.noreply.github.com>
2024-04-28 11:13:21 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 // jest.config.js
 module.exports = {
-    testEnvironment: 'node', // Specifies the environment in which the test should run
-    setupFilesAfterEnv: ['<rootDir>/src/setUpTests.js'], // Load some setup code for setting globals or configuring libraries
+    testEnvironment: 'node', 
+    setupFilesAfterEnv: ['<rootDir>/src/setUpTests.js'], 
     testMatch: [
         '**/__tests__/**/*.js?(x)', // Looks for any files inside __tests__ folders that end with .js or .jsx
-        '**/?(*.)+(spec|test).js?(x)' // Looks for any files ending in .test.js, .test.jsx, .spec.js, or .spec.jsx
+        '**/?(*.)+(spec|test).js?(x)' // Looks for any files ending in 
     ],
-    testPathIgnorePatterns: ['/node_modules/'], // Ignore tests from node_modules or any specified path
-    verbose: true, // Display individual test results with the test suite hierarchy.
+    testPathIgnorePatterns: ['/node_modules/'], //Ignore tests from node_modules or any specified path
+    verbose: true, //Display individual test results with the test suite hierarchy.
 };
